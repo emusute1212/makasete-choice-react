b011661e35d5bc79f54ea468f6a6b3cb5c84cd5d
宮西洋輔 <miyanishi1212@gmail.com>
2019-12-17 00:58:08 +0900

    feat: actionsの実装
    - Reduxのサンプルを参考に実装

---
+++ src/store/makasetechoice/actions.ts
@@ -0,0 +1,26 @@
+import {ADD_MEMBER, CHOICE_MEMBER, DELETE_MEMBER, MakaseteChoiceActionTypes, Member} from "./types";
+
+export function sendMember(newMember: Member): MakaseteChoiceActionTypes {
+    return {
+        type: ADD_MEMBER,
+        payload: newMember
+    }
+}
+
+export function deleteMember(targetNumber: number): MakaseteChoiceActionTypes {
+    return {
+        type: DELETE_MEMBER,
+        meta: {
+            number: targetNumber
+        }
+    }
+}
+
+export function choiceMember(splitNumber: number): MakaseteChoiceActionTypes {
+    return {
+        type: CHOICE_MEMBER,
+        meta: {
+            splitNumber
+        }
+    }
+}
